c9ddc6bd67b0529e696ce881c9f94c5b69ac5cde
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-06-09 10:49:45 +0700

    tambahkan handling exception untuk akses array

--- exptionStandar.cpp
+++ exptionStandar.cpp
@@ -10,4 +10,9 @@ int main(){
         array<int,3> data ={1,2,3};
         cout<<data.at(5)<<endl;
     }
+    catch (exception& e){
+        cout << e.what() <<endl;
+    }
+    cout <<"Baris program yang terakhir" << endl;
+    return 0;
 }
