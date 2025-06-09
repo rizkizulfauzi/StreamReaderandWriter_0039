05f776461a955d0fe001c1a586235af040a3a227
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-06-09 10:15:12 +0700

    melakukan penggulangan setiap baris

--- ioFile.cpp
+++ ioFile.cpp
@@ -6,20 +6,38 @@ using namespace std;
 int main()
 {
     string baris;
+
     ofstream outfile;
+
     outfile.open("contohFile.txt");
+
     cout << ">= Menulis file,\'q\' untuk keluar" << endl;
 
     while (true)
     {
         cout << "-";
+
         getline(cin, baris);
-        if (baris == "q") break;
+
+        if (baris == "q")
+            break;
+
         outfile << baris << endl;
     }
     outfile.close();
-    ifstream infile;
-    infile.open("contohFile.txt");
-    cout << endl << ">= Membuka dan membaca file" << endl;
-}
 
+    ifstream infile;
+
+    infile.open("contohFile.txt");
+
+    cout << endl
+         << ">= Membuka dan membaca file" << endl;
+
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            
+        }
+    }
+}
